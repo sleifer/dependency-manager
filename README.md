042182aa641b75514297f8f56677289933836e6a
Simeon Leifer <simeon@droolingcat.com>
2017-09-14 17:22:49 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dependency-manager
+
+A tool I wrote to help me manage versioned dependencies included as submodules in projects.
+
+Publishing in 2021 so I can share a few tools.
+
+Released under the MIT License.
---
+++ dependency-manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  dependency-manager
+//
+//  Created by Simeon Leifer on 9/14/17.
+//  Copyright © 2017 droolingcat.com. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
