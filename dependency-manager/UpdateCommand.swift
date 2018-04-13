04b0ad15973f84d356b2484d59c2bc120da44750
Simeon Leifer <simeon@droolingcat.com>
2018-04-13 07:06:47 -0700

    add missing support for updating a single module

--- dependency-manager/UpdateCommand.swift
+++ dependency-manager/UpdateCommand.swift
@@ -12,39 +12,49 @@ class UpdateCommand: Command {
     override func run(cmd: ParsedCommand) {
         let submodules = scm.submodules()
         for submodule in submodules {
-            print("submodule: \(submodule.name)")
-            print("  path: \(submodule.path)")
-            print("  current version: \(submodule.version)")
-
-            var newver: SemVer? = nil
-            if let spec = versionSpecs.spec(forName: submodule.name), let semver = spec.semver {
-                print("  spec: \(spec.versSpecStr())")
-
-                let result = scm.fetch(submodule.path)
-                if case .error(_, let text) = result {
-                    print(text)
-                }
-
-                let tags = scm.tags(submodule.path)
-                let matching = semver.matching(fromList: tags, withTest: spec.comparison)
-                if let last = matching.last {
-                    if submodule.semver == nil {
-                        newver = last
-                    } else if let cursemver = submodule.semver, last > cursemver {
-                        newver = last
-                    }
-                    if let newver = newver {
-                        print("  Updating to version: \(newver.fullString)")
-                        scm.checkout(submodule.path, object: newver.fullString)
-                    } else {
-                        print("  Up to date.")
-                    }
+            var updateIt: Bool = true
+            if cmd.parameters.count != 0 {
+                if cmd.parameters.contains(submodule.name) == true {
+                    updateIt = true
                 } else {
-                    print("  No versions matching spec found.")
+                    updateIt = false
                 }
             }
+            if updateIt == true {
+                print("submodule: \(submodule.name)")
+                print("  path: \(submodule.path)")
+                print("  current version: \(submodule.version)")
 
-            print()
+                var newver: SemVer? = nil
+                if let spec = versionSpecs.spec(forName: submodule.name), let semver = spec.semver {
+                    print("  spec: \(spec.versSpecStr())")
+
+                    let result = scm.fetch(submodule.path)
+                    if case .error(_, let text) = result {
+                        print(text)
+                    }
+
+                    let tags = scm.tags(submodule.path)
+                    let matching = semver.matching(fromList: tags, withTest: spec.comparison)
+                    if let last = matching.last {
+                        if submodule.semver == nil {
+                            newver = last
+                        } else if let cursemver = submodule.semver, last > cursemver {
+                            newver = last
+                        }
+                        if let newver = newver {
+                            print("  Updating to version: \(newver.fullString)")
+                            scm.checkout(submodule.path, object: newver.fullString)
+                        } else {
+                            print("  Up to date.")
+                        }
+                    } else {
+                        print("  No versions matching spec found.")
+                    }
+                }
+
+                print()
+            }
         }
     }
 }
