2b1bd6f322ec39ada02f30d77a2e7c14eb393002
Simeon Leifer <simeon@droolingcat.com>
2019-02-02 11:16:38 -0800

    Updated CommandLineCore to 1.4.3

--- dependency-manager/main.swift
+++ dependency-manager/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CommandLineCore
 
-let toolVersion = "0.33"
+let toolVersion = "0.34"
 let versionSpecsFileName = ".module-versions"
 let scm: SCM = Git()
 var versionSpecs = VersionSpecification()
